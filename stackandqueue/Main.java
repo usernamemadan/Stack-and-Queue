f4eca474f60375a7b0ca1fc7e4fc83bcc0f916eb
madan <madan@local.com>
2021-09-14 00:10:32 +0530

    added queue class to store the queue data

--- stackandqueue/Main.java
+++ stackandqueue/Main.java
@@ -2,7 +2,7 @@ package com.stackandqueue;
 
 public class Main {
 	public static void main(String[] args) {
-		StackQueue stack = new StackQueue();
+		Stack stack = new Stack();
 		stack.push(56);
 		stack.push(30);
 		stack.push(70);
@@ -14,5 +14,14 @@ public class Main {
 	    
 	    int peek = stack.peek();
 	    System.out.println("top element : "+peek);
+	    
+	    Queue q = new Queue();
+	    q.enqueue(56);
+	    q.enqueue(30);
+	    q.enqueue(70);
+	    q.print();
+	    System.out.println();
+	    
+	 
 	}
 }
---
+++ stackandqueue/Queue.java
@@ -0,0 +1,51 @@
+package com.stackandqueue;
+
+public class Queue {
+private Node head;
+	
+	private class Node {
+		int data;
+		Node next;
+		
+		Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	/**to append the elements to the queue
+	 */
+	public boolean enqueue(int data) {
+		boolean isAdded  = false;
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+			isAdded  =true;
+		}else {
+			Node temp = head;
+			
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			
+			temp.next  = newNode;
+			isAdded = true;
+		}
+		return isAdded;
+	}
+	
+	/**to print the element of the queue
+	 */
+	public void print() {
+		Node temp = head;
+		
+		System.out.println("Queue : ");
+		
+		while(temp != null) {
+			System.out.print(temp.data + "   ");
+			temp = temp.next;
+		}
+	}
+	
+	
+}
